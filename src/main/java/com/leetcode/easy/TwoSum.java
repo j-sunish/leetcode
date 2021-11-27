1c4af4deba181e18189d21e4bec1c24770996296
sunish jose <jsunish@gmail.com>
2021-11-27 03:08:48 +0000

    Two Sum Commit

---
+++ src/main/java/com/leetcode/easy/TwoSum.java
@@ -0,0 +1,26 @@
+package com.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. Two Sum
+ * 
+ * @author Sunish
+ *
+ */
+public class TwoSum {
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		int[] rst = new int[2];
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(target - nums[i])) {
+				rst[0] = i;
+				rst[1] = map.get(target - nums[i]);
+				break;
+			}
+			map.put(nums[i], i);
+		}
+		return rst;
+	}
+}
